32ff53e41e229ea3a9561c56ae808b44de0cdd8b
ASADI VAMSI <165474708+ASADIVAMSI@users.noreply.github.com>
2026-07-09 13:00:57 +0530

    Add UPDATE Operation

--- java-app-09042026/src/program/MainProgram1.java
+++ java-app-09042026/src/program/MainProgram1.java
@@ -1,98 +1,60 @@
+/*
+====================================
+
+            JDBC
+
+           UPDATE
+
+====================================
+*/
+
+
 package program;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class MainProgram1 {
+public class MainProgram3 {
 	public static void main(String[] args) {
+		
 		Connection dbConn = null;
 		Statement dbStmt = null;
-		ResultSet dbRs = null;
 		
 		try {
+			
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			dbConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jfs_049","root","root");
 			dbStmt = dbConn.createStatement();
 			
-/* we know that result set have only one record that's why no need to write condtion here*/
-//			String sqlQuery = "SELECT * FROM employee WHERE empId = 104";
-//			dbRs  = dbStmt.executeQuery(sqlQuery);
-//			
-//			    dbRs.next();
-//				int empId = dbRs.getInt(1);
-//				String firstName = dbRs.getString(2);
-//				String lastName  = dbRs.getString(3);
-//				
-//				System.out.println("Employee Id: "+empId);
-//				System.out.println("First Name : "+firstName);
-//				System.out.println("Last Name : "+lastName);
-//				System.out.println();
+			String sqlQuery ="UPDATE employees_list SET login_password ='123'WHERE emp_id =103";
+			
+			int numRowsAffeted = dbStmt.executeUpdate(sqlQuery);
+			
+			if(numRowsAffeted != 0) {
+				System.out.println("Record Updated");
+			}else {
+				System.out.println("Record Not updated");
+			}
 			
 			
-
-//				String sqlQuery = "SELECT * FROM employee";
-//				dbRs  = dbStmt.executeQuery(sqlQuery);
-				
-//			while(dbRs.next()) {
-//			
-//				int empId = dbRs.getInt(1);
-//				String firstName = dbRs.getString(2);
-//				String lastName  = dbRs.getString(3);
-//				
-//				System.out.println("Employee Id: "+empId);
-//				System.out.println("First Name : "+firstName);
-//				System.out.println("Last Name : "+lastName);
-//				System.out.println();
-//			}
-				
-			
-//			
-//				String sqlQuery = "SELECT * FROM employee WHERE empId = 109";
-//				dbRs  = dbStmt.executeQuery(sqlQuery);
-//				
-//				if(dbRs.next()) {
-//					System.out.println("Employee found given Id.");
-//					int empId = dbRs.getInt(1);
-//					String firstName = dbRs.getString(2);
-//					String lastName  = dbRs.getString(3);
-//					
-//					System.out.println("Employee Id: "+empId);
-//					System.out.println("First Name : "+firstName);
-//					System.out.println("Last Name : "+lastName);
-//					System.out.println();
-//				}else {
-//					System.out.println("Employee not found given Id.");
-//				}
-				
-			
-			
-			
-		}catch(ClassNotFoundException cnfEx) {
-			cnfEx.printStackTrace();
-		}catch(SQLException sqlEx) {
-			sqlEx.printStackTrace();
-		}finally {
+		}catch(ClassNotFoundException | SQLException ex) {
+			ex.printStackTrace();
+		}finally {    //Adding Finally block 
 			
 			try {
-				if(dbRs !=null) {
-					dbRs.close();
-					
-				}
-				if(dbStmt !=null) {
-					dbStmt.close();
-					
-				}
-				if(dbConn !=null) {
+				if(dbConn != null) {
 					dbConn.close();
-					
 				}
-				
+				if(dbStmt != null) {
+					dbStmt.close();
+				}
 			}catch(SQLException sqlEx) {
 				sqlEx.printStackTrace();
 			}
 		}
+		
+		
 	}
 }
