44faa460edbaf1c4e83fb212e992824fa75021fa
ASADI VAMSI <165474708+ASADIVAMSI@users.noreply.github.com>
2026-07-15 12:47:06 +0530

    Add UPDATE Operation PreStmt1

--- java-app-09042026/src/program/MainProgram3.java
+++ java-app-09042026/src/program/MainProgram3.java
@@ -1,31 +1,50 @@
-package program;
+/*
+	====================================
+	
+	            JDBC
+	
+	      UPDATE - Static Values
+	      
+	 (PrepareStatement-Same Query Multiple Times)
+	
+	====================================
+*/
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class MainProgram3 {
 	public static void main(String[] args) {
 		
 		Connection dbConn = null;
-		Statement dbStmt = null;
+		PreparedStatement dbPreStmt = null;
 		
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			// Step 1: Load Driver (optional in newer JDBC versions)
+			Class.forName("com.msql.cj.jdbc.Driver");
+			
+			// Step 2: Create Connection
 			dbConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jfs_049","root","root");
-			dbStmt = dbConn.createStatement();
-
 			
-			String sqlQuery = "UPDATE employee SET firstName = 'Kumar' WHERE empId = 103"; 
-			int numRowsAffeted = dbStmt.executeUpdate(sqlQuery);
+			// Step 3: SQL Query
+			String sqlQuery ="UPDATE employees_list SET first_name = ? where id = ? ";
 			
-			if(numRowsAffeted != 0) {
+			// Step 4: Create PreparedStatement
+			dbPreStmt = dbConn.prepareStatement(sqlQuery);
+			
+			// Step 5: Set Values
+			dbPreStmt.setString(1,"raamu");
+			dbPreStmt.setInt(2,105);
+			
+			int noOfRowsAff = dbPreStmt.executeUpdate();
+			
+			if(noOfRowsAff != 0) {
 				System.out.println("Record Updated.");
 			}else {
 				System.out.println("Not Updated.");
 			}
-			
 			
 			
 		}catch(ClassNotFoundException cnfEx) {
@@ -35,8 +54,8 @@ public class MainProgram3 {
 		}finally {
 			
 			try {
-				if(dbStmt !=null) {
-					dbStmt.close();
+				if(dbPreStmt !=null) {
+					dbPreStmt.close();
 					
 				}
 				if(dbConn !=null) {
@@ -47,7 +66,6 @@ public class MainProgram3 {
 			}catch(SQLException sqlEx) {
 				sqlEx.printStackTrace();
 			}
-		}
+		}		
 	}
-
 }
